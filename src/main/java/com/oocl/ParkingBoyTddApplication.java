823cb1198e051ff92e4c48267f4dfaa29561f3e4
LEON YU (ITA-ISDC-ISD-OOCLL/ZHA) <YULE4@OOCL.COM>
2018-08-23 15:30:38 +0800

    [Leon,Jerry]first commmit

---
+++ src/main/java/com/oocl/ParkingBoyTddApplication.java
@@ -0,0 +1,12 @@
+package com.oocl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingBoyTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingBoyTddApplication.class, args);
+	}
+}
